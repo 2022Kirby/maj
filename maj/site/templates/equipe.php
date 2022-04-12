6bbcc91728cba456ce4a202c6127cf5e26c3d889
Yasmeen Lefebvre <2022kirby@gmail.com>
2022-04-12 17:36:45 +0200

    maj page équipe

--- maj/site/templates/equipe.php
+++ maj/site/templates/equipe.php
@@ -4,21 +4,36 @@
     <?php snippet('header') ?>
     
     <main>
-        <div class="equipe">
+        <div id="equipe">
+
             <!-- boucle affichant pour chaque membre de la page équipe, les champs prénom, nom, détail et téléphone -->
             <?php foreach($page->membres()->toStructure() as $membre):?>
-                <p><?= $membre->prenom() ?> <span><?= $membre->nom() ?></span></p>
-                
-                <!-- < ?php $nameService = str_replace(' ','-',strtolower($membre->service())); ?>
-                    <a href="< ?= $pages->find('services')->url() ?>/< ?= $nameService ?>">
-                        < ?= $membre->service() ?>
-                    </a>
-                < ?php ?>
-                <p>< ?= $membre->detail() ?></p>
-                lien vers page de contact avec ajout d'un paramètre service dans l'url
-                donne l'url suivante: http://maj.test/contact?service=Nom Service
-                <a href="< ?= $pages->find('contact')->url() ?>?service=< ?= $membre->service() ?>">Formulaire de contact</a> -->
+                <div class="membre">
+                    <!-- prenom nom -->
+                    <h1><?= $membre->prenom() ?> <span><?= $membre->nom() ?></span></h1>
+                    
+                    <div class="infos">
+                        <!-- poste -->
+                        <p><b><?= $membre->poste() ?></b></p>
+                        
+                        <p>
+                            <!-- lien vers la page de service -->
+                            <?php $nameService = str_replace(' ','-',strtolower($membre->service())); ?>
+                                <a href="<?= $pages->find('services')->find($nameService)->url() ?>">
+                                    <?= $membre->service() ?>
+                                </a>
+                            <?php ?>
+
+                                <span>&nbsp;|&nbsp;</span>
+                                
+                                <!-- lien vers page de contact avec ajout d'un paramètre service dans l'url
+                                donne l'url suivante: http://maj.test/contact?service=Nom Service -->
+                                <a href="<?= $pages->find('contact')->url() ?>?service=<?= $membre->service() ?>">Contacter</a>
+                        </p>
+                    </div>
+                </div>
             <?php endforeach ?>
+
         </div>
     </main>
 
