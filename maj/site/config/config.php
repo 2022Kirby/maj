4541e18325b597e9abd6a472ed9c430ae11087e5
Yasmeen Lefebvre <2022kirby@gmail.com>
2022-04-27 10:44:38 +0200

    maj config

--- maj/site/config/config.php
+++ maj/site/config/config.php
@@ -1,9 +1,25 @@
 <?php
-return [
-    'debug'  => true,
-    'markdown' => [
-        'extra' => true,
-        'breaks' => true,
-    ]
-];
+    return [
+        'debug'  => true,
+        'markdown' => [
+            'extra' => true,
+            'breaks' => true,
+        ],
+        'panel' => [
+            'kirbytext' => [
+                'fileDragText' => function(Kirby\Cms\File $file, $url) {
+                    // if($file->type() === 'image') {
+                    //     return sprintf('(image: %s text: %s)', $url, $file->title()->or('Some fallback text')) ;
+                    // }
+
+                    if($file->type() === 'document') {
+                        return sprintf('(file: %s text: %s)', $url, $file->alt()->or($file->name()));
+                    }
+
+                    return null;
+                }
+
+            ],
+        ],
+    ];
 ?>
